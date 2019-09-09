9f8080c635dc29674e54005d5abe2d28f6ee363c
edevans <edevans@iastate.edu>
2019-09-08 22:37:53 -0500

    inital commit

---
+++ Experiments/Ethan/demoproject/SampleProjcetApplication.java
@@ -0,0 +1,13 @@
+package com.javadev.demoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProjcetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleProjcetApplication.class, args);
+    }
+
+}
