05641e76d829a973d71d0b02883aad6ba57587a8
unknown <amdort@iastate.edu>
2019-09-09 11:50:43 -0500

    Fixed gradle issues and replaced code

--- Experiments/Andrew/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Experiments/Andrew/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
 
 @SpringBootApplication
 public class DemoApplication {
