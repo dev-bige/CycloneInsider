a6306d3482e2f7fc6be46103afaa65ae5e4be0f4
cpf6692 <cpf6692@iastate.edu>
2019-09-10 13:24:02 -0500

    Added a sign-in page, can tell if user does not enter anything into the net-id or password section

--- Frontend/app/src/main/java/edu/cs309/cycloneinsider/MainActivity.java
+++ Frontend/app/src/main/java/edu/cs309/cycloneinsider/MainActivity.java
@@ -2,7 +2,10 @@ package edu.cs309.cycloneinsider;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void openMainPage(View view){
+
+        Intent intent = new Intent(this, OpenMainPage.class); //starts an indent
+        EditText netID = (EditText) findViewById(R.id.net_id); //finds the user name in netID box by ID
+        EditText password = (EditText) findViewById(R.id.password); //finds the password in password box by ID
+        EditText hiddenText = (EditText) findViewById(R.id.hidden_text); //finds the hidden text box that communicates errors to user when entering wrong values
+        hiddenText.setVisibility(View.INVISIBLE); //makes sure to have the text box INVISIBLE, unless a conditional statement triggers it to be VISIBLE
+        String netIDString = netID.getText().toString(); //extracts the string of the netID
+        String passwordString = password.getText().toString(); //extracts the string of the password
+        String hiddenTextString = hiddenText.getText().toString(); //extracts the string value of hiddenText in order to set this value when users enter bad username/passwords
+        System.out.println("NetID "+netIDString.length());
+        System.out.println("Password "+passwordString.length());
+
+        if(netIDString.length() == 0 && passwordString.length() == 0){ //Net ID and Password must be entered
+
+            hiddenText.getText().clear();
+            hiddenText.setText("You must enter your Net ID and Password");
+            hiddenText.setVisibility(View.VISIBLE);
+            return;
+
+        }
+
+        if(netIDString.length() == 0){ //Net ID must be entered
+
+            hiddenText.getText().clear();
+            hiddenText.setText("You must enter your Net ID");
+            hiddenText.setVisibility(View.VISIBLE);
+            return;
+
+        }
+
+        if(passwordString.length() == 0){ //Password must be entered
+
+            hiddenText.getText().clear();
+            hiddenText.setText("You must enter your Password");
+            hiddenText.setVisibility(View.VISIBLE);
+            return;
+
+        }
+
+       startActivity(intent); //opens up the main page
+
     }
 }
---
+++ Frontend/app/src/main/java/edu/cs309/cycloneinsider/OpenMainPage.java
@@ -0,0 +1,20 @@
+package edu.cs309.cycloneinsider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class OpenMainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.open_main_page);
+    }
+
+
+
+}
