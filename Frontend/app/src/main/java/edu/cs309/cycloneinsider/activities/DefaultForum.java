7f33ead1fcfbd6a85036a4b68bf8c2c076a9bddb
cpf6692 <cpf6692@iastate.edu>
2019-09-22 16:41:41 -0500

    Just made the database for the swear words and added its functionality. Now I need to add for it to only trigger when the filter is set.

--- Frontend/app/src/main/java/edu/cs309/cycloneinsider/activities/DefaultForum.java
+++ Frontend/app/src/main/java/edu/cs309/cycloneinsider/activities/DefaultForum.java
@@ -4,22 +4,116 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import edu.cs309.cycloneinsider.R;
 
 public class DefaultForum extends AppCompatActivity {
+    private HashMap<String,Integer> dict = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.default_forum);
+        ExplicitWordMap(); //initializes the explicit words in the Map
+
+    }
+
+    private void ExplicitWordMap()
+    {
+        //String of all the swear words we are using
+      String swearWordList =   "anal\n" +
+                "anus\n" +
+                "arse\n" +
+                "ass\n" +
+                "ballsack\n" +
+                "balls\n" +
+                "bastard\n" +
+                "bitch\n" +
+                "biatch\n" +
+                "bloody\n" +
+                "blowjob\n" +
+                "bollock\n" +
+                "bollok\n" +
+                "boner\n" +
+                "boob\n" +
+                "bugger\n" +
+                "bum\n" +
+                "butt\n" +
+                "buttplug\n" +
+                "clitoris\n" +
+                "cock\n" +
+                "coon\n" +
+                "crap\n" +
+                "cunt\n" +
+                "damn\n" +
+                "dick\n" +
+                "dildo\n" +
+                "dyke\n" +
+                "fag\n" +
+                "feck\n" +
+                "fellate\n" +
+                "fellatio\n" +
+                "felching\n" +
+                "fuck\n" +
+                "fudgepacker\n" +
+                "flange\n" +
+                "Goddamn\n" +
+                "God damn\n" +
+                "hell\n" +
+                "homo\n" +
+                "jerk\n" +
+                "jizz\n" +
+                "knobend\n" +
+                "knob end\n" +
+                "labia\n" +
+                "lmao\n" +
+                "lmfao\n" +
+                "muff\n" +
+                "nigger\n" +
+                "nigga\n" +
+                "omg\n" +
+                "penis\n" +
+                "piss\n" +
+                "poop\n" +
+                "prick\n" +
+                "pube\n" +
+                "pussy\n" +
+                "queer\n" +
+                "scrotum\n" +
+                "sex\n" +
+                "shit\n" +
+                "sh1t\n" +
+                "slut\n" +
+                "smegma\n" +
+                "spunk\n" +
+                "tit\n" +
+                "tosser\n" +
+                "turd\n" +
+                "twat\n" +
+                "vagina\n" +
+                "wank\n" +
+                "whore\n" +
+                "wtf";
+
+      //puts all of the swear words into a HashMap
+      Scanner scan = new Scanner(swearWordList);
+      while(scan.hasNextLine()){
+
+          String word = scan.nextLine();
+          dict.put(word,1);
+
+      }
 
     }
 
     public void PostThread(View view) {
+
         EditText postTitle = (EditText) findViewById(R.id.post_title); //finds the post title by ID
         EditText initialText = (EditText) findViewById(R.id.poster_comment); //finds the initial comment from poster by ID
         String title = postTitle.getText().toString(); //title of thread as a string
@@ -27,7 +121,8 @@ public class DefaultForum extends AppCompatActivity {
         String hidden = hiddenText.getText().toString(); //gets the hidden text box as a string
         hiddenText.setVisibility(View.INVISIBLE); //initially has hidden text box invisible
         String initialComments = initialText.getText().toString(); //posters initial comments as a string
-
+        String[] titleWords = title.split(" ",0);
+        String[] initialCommentsWords = initialComments.split(" ",0);
         if (title.length() == 0 && initialComments.length() == 0) { //poster must enter a title and have an initial comment
 
             hiddenText.setText("Need to enter a title and place an initial comment");
@@ -85,6 +180,32 @@ public class DefaultForum extends AppCompatActivity {
             hiddenText.setVisibility(View.VISIBLE);
             return;
         }
+
+        for (int i = 0; i < titleWords.length; i++) {
+
+            if (dict.containsKey(titleWords[i].toLowerCase())) {
+
+                hiddenText.setText("Cannot have explicit word: '" + titleWords[i] + "' in the title");
+                hiddenText.setVisibility(View.VISIBLE);
+                return;
+
+            }
+
+        }
+
+        for (int i = 0; i < initialCommentsWords.length; i++) {
+
+            if (dict.containsKey(initialCommentsWords[i].toLowerCase())) {
+
+                hiddenText.setText("Cannot have explicit word: '" + initialCommentsWords[i] + "' in the initial comments");
+                hiddenText.setVisibility(View.VISIBLE);
+                return;
+
+            }
+        }
+
+
+
 
         //TODO intent goes here
 
