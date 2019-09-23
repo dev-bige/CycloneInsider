5ec896e1b456b0ff18cb9ca107886432f7b6e1d0
edevans <edevans@iastate.edu>
2019-09-22 23:21:15 -0500

    Working on making it so posts are shown on main page

---
+++ Frontend/app/src/main/java/edu/cs309/cycloneinsider/activities/GetPost.java
@@ -0,0 +1,25 @@
+package edu.cs309.cycloneinsider.activities;
+
+
+
+import java.util.ArrayList;
+import java.util.Observable;
+
+
+import edu.cs309.cycloneinsider.CycloneInsiderApp;
+import edu.cs309.cycloneinsider.api.CycloneInsiderService;
+import edu.cs309.cycloneinsider.api.models.LoginRequestModel;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class GetPost extends Observable {
+
+
+    CycloneInsiderService cycloneInsiderService = new CycloneInsiderApp().getApiService();
+
+
+
+
+
+
+}
