ab984cc0474dfb4ab5c83d7847d66381609614b8
adamcorp <adamcorp@iatate.edu>
2019-09-25 14:36:57 -0500

    create template for loading posts into a list.

--- Frontend/app/src/main/java/edu/cs309/cycloneinsider/fragments/PostListFragment.java
+++ Frontend/app/src/main/java/edu/cs309/cycloneinsider/fragments/PostListFragment.java
@@ -11,12 +11,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.List;
+
 import edu.cs309.cycloneinsider.R;
 import edu.cs309.cycloneinsider.activities.DefaultForum;
+import edu.cs309.cycloneinsider.activities.InsiderActivity;
+import edu.cs309.cycloneinsider.api.models.PostModel;
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import retrofit2.Response;
 
 public class PostListFragment extends Fragment {
     public static final String ROOM_UUID = "ROOM_UUID";
     private String roomUUID;
+    private Disposable postSub;
 
     public static PostListFragment newInstance(String roomUuid) {
         PostListFragment postListFragment = new PostListFragment();
@@ -46,5 +54,32 @@ public class PostListFragment extends Fragment {
         });
         TextView roomUUIdTextView = getView().findViewById(R.id.room_uuid);
         roomUUIdTextView.setText(roomUUID);
+        Observable<Response<List<PostModel>>> postListObservable = null;
+        //If the room uuid is null then we should get the front page posts.
+        if (roomUUID == null) {
+            postListObservable = ((InsiderActivity) getActivity())
+                    .getInsiderApplication()
+                    .getApiService()
+                    .getFrontPagePosts();
+        } else {
+            //TODO GET POSTS FOR ROOM UUID... for now, just have an empty steam so we don't crash.
+            postListObservable = Observable.empty();
+        }
+
+        postSub = postListObservable.subscribe(postsResponse -> {
+            if (postsResponse.isSuccessful()) {
+                List<PostModel> posts = postsResponse.body();
+                //TODO Do something with posts
+            }
+        });
+
+    }
+
+    @Override
+    public void onDestroy() {
+        if (!postSub.isDisposed()) {
+            postSub.dispose();
+        }
+        super.onDestroy();
     }
 }
