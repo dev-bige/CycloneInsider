dbfd502f217295d198778833799a1d20d79a0e7d
Andrew Dort <amd@acgolter.student.iastate.edu>
2019-09-27 19:25:49 -0500

    CommentRepo,Controller,updates

--- Backend/src/main/java/edu/cyclone/insider/repos/CommentsRepository.java
+++ Backend/src/main/java/edu/cyclone/insider/repos/CommentsRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 import java.util.UUID;
 
-public interface CommentsRepository extends JpaRepository<Post, Comments> {
+public interface CommentsRepository extends JpaRepository<Comments, UUID> {
     @Query(value = "SELECT * from comments p where p.post_uuid = :post_uuid", nativeQuery = true)
     List<Comments> getComment();
 
