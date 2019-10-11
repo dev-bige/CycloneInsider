9e4df39112ce97bab6bb37af3e45dac2ce19c981
adamcorp <adamcorp@iastate.edu>
2019-10-10 23:54:35 -0500

    API - update post controller

--- Backend/src/main/java/edu/cyclone/insider/controllers/post/PostController.java
+++ Backend/src/main/java/edu/cyclone/insider/controllers/post/PostController.java
@@ -8,24 +8,31 @@ package edu.cyclone.insider.controllers.post;
 import edu.cyclone.insider.controllers.BaseController;
 import edu.cyclone.insider.controllers.post.models.PostCreateRequestModel;
 import edu.cyclone.insider.models.Post;
+import edu.cyclone.insider.models.Room;
 import edu.cyclone.insider.repos.PostRepository;
+import edu.cyclone.insider.repos.RoomRepository;
 import edu.cyclone.insider.repos.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("posts")
 public class PostController extends BaseController {
     private PostRepository postRepository;
+    private RoomRepository roomRepository;
 
     @Autowired
-    public PostController(PostRepository postRepository, UsersRepository usersRepository) {
+    public PostController(PostRepository postRepository, UsersRepository usersRepository, RoomRepository roomRepository) {
         super(usersRepository);
         this.postRepository = postRepository;
+        this.roomRepository = roomRepository;
     }
 
     @RequestMapping(value = "front-page", method = RequestMethod.GET)
@@ -35,12 +42,12 @@ public class PostController extends BaseController {
 
     @RequestMapping(value = "front-page", method = RequestMethod.POST)
     public Post postFrontPagePost(@RequestBody PostCreateRequestModel request) {
-        return createPost(request);
+        return createPost(request, null);
     }
 
     @RequestMapping(value = "{roomUuid}", method = RequestMethod.POST)
     public Post postToRoom(@PathVariable("roomUuid") UUID roomUuid, @RequestBody PostCreateRequestModel request) {
-        return createPost(request);
+        return createPost(request, roomUuid);
     }
 
     @RequestMapping(value = "{roomUuid}", method = RequestMethod.GET)
@@ -48,10 +55,14 @@ public class PostController extends BaseController {
         return postRepository.getPostsByRoom(roomUuid);
     }
 
-    private Post createPost(@RequestBody PostCreateRequestModel request) {
+    private Post createPost(@RequestBody PostCreateRequestModel request, UUID roomUUid) {
+        Optional<Room> byId = roomRepository.findById(roomUUid);
+            if (!byId.isPresent() && roomUUid != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
         Post post = new Post();
         post.setContent(request.content);
-        post.setRoom(null);
+        post.setRoom(roomUUid == null ? null : byId.get());
         post.setUser(getCurrentUser());
         post.setTags(request.tags);
         post.setTitle(request.title);
