399167ab986c22bb68c50053560be607578d0db5
adamcorp <adamcorp@iastate.edu>
2019-10-20 13:48:10 -0500

    api - move inner class outside

--- Backend/src/test/java/ExampleTest.java
+++ Backend/src/test/java/ExampleTest.java
@@ -1,52 +1,15 @@
 import edu.cyclone.insider.models.InsiderUser;
 import edu.cyclone.insider.repos.UsersRepository;
-import static org.assertj.core.api.Assertions.*;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.*;
 public class ExampleTest {
-    @RunWith(SpringRunner.class)
-    public static class InsiderUserServiceImplIntegrationTest {
-
-        @TestConfiguration
-        static class InsiderUserServiceImplTestContextConfiguration {
-
-            @Bean
-            public InsiderUser insiderUser() {
-                return new InsiderUser();
-            }
-        }
-
-        @Autowired
-        private InsiderUser insiderUser;
-
-        @MockBean
-        private UsersRepository usersRepository;
-        @Before
-        public void setUp() {
-            InsiderUser timmy = new InsiderUser();
-           timmy.setUsername("LilTimTim");
-
-            Mockito.when(usersRepository.findUserByUsername(timmy.getUsername()))
-                    .thenReturn(timmy);
-        }
-        @Test
-        public void whenValidName_thenUserShouldBeFound() {
-            InsiderUser timmy= new InsiderUser() ;
-            timmy.setUsername("LilTimTim");
-            String username = "LilTimTim";
-
-            assertThat(timmy.getUsername())
-                    .isEqualTo(username);
-        }
-    }
 }
