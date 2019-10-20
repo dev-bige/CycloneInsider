847720324410195b1cced9472e6f2b622c7b916b
Andrew Dort <amdort@iastate.edu>
2019-10-20 13:32:29 -0500

    First test to test username retrieval

--- Backend/src/test/java/ExampleTest.java
+++ Backend/src/test/java/ExampleTest.java
@@ -1,9 +1,52 @@
-import org.junit.Test;
+import edu.cyclone.insider.models.InsiderUser;
+import edu.cyclone.insider.repos.UsersRepository;
+import static org.assertj.core.api.Assertions.*;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
 public class ExampleTest {
-    @Test
-    public void exampleTest() {
-        assertEquals(true, true);
+    @RunWith(SpringRunner.class)
+    public static class InsiderUserServiceImplIntegrationTest {
+
+        @TestConfiguration
+        static class InsiderUserServiceImplTestContextConfiguration {
+
+            @Bean
+            public InsiderUser insiderUser() {
+                return new InsiderUser();
+            }
+        }
+
+        @Autowired
+        private InsiderUser insiderUser;
+
+        @MockBean
+        private UsersRepository usersRepository;
+        @Before
+        public void setUp() {
+            InsiderUser timmy = new InsiderUser();
+           timmy.setUsername("LilTimTim");
+
+            Mockito.when(usersRepository.findUserByUsername(timmy.getUsername()))
+                    .thenReturn(timmy);
+        }
+        @Test
+        public void whenValidName_thenUserShouldBeFound() {
+            InsiderUser alex = new InsiderUser() ;
+            alex.setUsername("LilTimTim");
+            String username = "LilTimTim";
+
+            assertThat(alex.getUsername())
+                    .isEqualTo(username);
+        }
     }
 }
