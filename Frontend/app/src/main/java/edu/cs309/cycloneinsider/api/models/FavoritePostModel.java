9918bf212b83c4c0ed841e9e9a88a055fe4da2c9
adamcorp <adamcorp@iastate.edu>
2019-10-20 17:02:43 -0500

    ui - display date under title of post in list

--- Frontend/app/src/main/java/edu/cs309/cycloneinsider/api/models/FavoritePostModel.java
+++ Frontend/app/src/main/java/edu/cs309/cycloneinsider/api/models/FavoritePostModel.java
@@ -1,9 +1,12 @@
 package edu.cs309.cycloneinsider.api.models;
 
+import java.util.Date;
+
 public class FavoritePostModel {
     String uuid;
     InsiderUserModel user;
     PostModel post;
+    Date date;
 
     public String getUuid() {
         return uuid;
@@ -27,5 +30,13 @@ public class FavoritePostModel {
 
     public void setPost(PostModel post) {
         this.post = post;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
