8c8d5a233d86f8954ac388ed4376e60e946bfa61
Ethan Evans <edevans@iastate.edu>
2019-10-29 17:12:36 -0500

    Changing to MVVM for my post

--- Frontend/app/src/main/java/edu/cs309/cycloneinsider/viewmodels/MyPostListViewModel.java
+++ Frontend/app/src/main/java/edu/cs309/cycloneinsider/viewmodels/MyPostListViewModel.java
@@ -1,6 +1,41 @@
 package edu.cs309.cycloneinsider.viewmodels;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import edu.cs309.cycloneinsider.api.CycloneInsiderService;
+import edu.cs309.cycloneinsider.api.models.PostModel;
+import io.reactivex.Observable;
+import retrofit2.Response;
+
 public class MyPostListViewModel extends ViewModel {
+    private final MutableLiveData<Response<List<PostModel>>> myPostListResponse = new MutableLiveData<>();
+    private CycloneInsiderService cycloneInsiderService;
+
+    @Inject
+    public MyPostListViewModel(CycloneInsiderService cycloneInsiderService) {
+        this.cycloneInsiderService = cycloneInsiderService;
+    }
+
+    @Override
+    public void onCleared() {
+        super.onCleared();
+    }
+
+    public void refresh() {
+        Observable<Response<List<PostModel>>> observable = null;
+
+        observable = cycloneInsiderService.getMyPosts();
+
+        observable.subscribe(myPostListResponse::postValue);
+    }
+
+    public LiveData<Response<List<PostModel>>> getMyPostResponse() {
+        return myPostListResponse;
+    }
 }
