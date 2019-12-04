44af648903a0619edb2ff9cc363b3899ab5ac756
StrangeSeal <33080784+StrangeSeal@users.noreply.github.com>
2019-12-03 19:51:48 -0600

    more doc

--- Backend/src/main/java/edu/cyclone/insider/controllers/user/UserController.java
+++ Backend/src/main/java/edu/cyclone/insider/controllers/user/UserController.java
@@ -32,42 +32,68 @@ public class UserController {
         this.postsService = postsService;
         this.commentsService = commentsService;
     }
-
+    /**
+     * get user membershipts
+     *
+     * @return memberships
+     */
     @RequestMapping(value = "memberships", method = RequestMethod.GET)
     public List<RoomMembership> getUserMemberships() {
         return roomMembershipService.getMemberships();
     }
-
+    /**
+     * get pending memberships to a room
+     * @return list of pending memberships
+     */
     @RequestMapping(value = "memberships/pending", method = RequestMethod.GET)
     public List<RoomMembership> getPendingMemberships() {
         return roomMembershipService.getPendingMemberships();
     }
-
+    /**
+     * favorite posts
+     * @return all posts that the user has saved in favorites
+     */
     @RequestMapping(value = "current/favorite-posts", method = RequestMethod.GET)
     public List<FavPost> getFavPosts() {
         return favoritePostService.getFavPosts();
     }
-
+    /**
+     * get comments by user
+     * @return comments by current logged in user
+     */
     @RequestMapping(value = "current/users-comments", method = RequestMethod.GET)
     public List<Comment> getMyComments() {
         return commentsService.getCommentsByUser();
     }
-
+    /**
+     *get posts
+     * @return all posts by logged in user
+     */
     @RequestMapping(value = "current/users-posts", method = RequestMethod.GET)
     public List<Post> getMyPosts() {
         return postsService.getPostsByUser();
     }
-
+    /**
+     * get user
+     * @param username
+     * @return user
+     */
     @RequestMapping(value = "{username}/profile", method = RequestMethod.GET)
     public InsiderUser getUser(@PathVariable("username") String username) {
         return userService.findByUsername(username);
     }
-
+    /**
+     * get the current user
+     * @return the user that is logged in currently
+     */
     @RequestMapping(value = "current", method = RequestMethod.GET)
     public InsiderUser current() {
         return userStateService.getCurrentUser();
     }
-
+    /**
+     * sign up
+     * @return signed up user request
+     */
     @RequestMapping(value = "sign-up", method = RequestMethod.POST)
     public InsiderUser signUp(@RequestBody SignUpRequestModel request) {
         return userService.signUp(request);
